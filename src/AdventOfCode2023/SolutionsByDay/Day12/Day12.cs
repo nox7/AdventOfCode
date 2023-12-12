1a95385c100b54754c6d4f588731701871411710
Garet C. Green <17110935+nox7@users.noreply.github.com>
2023-12-12 11:08:52 -0600

    Day 12 solution

---
+++ src/AdventOfCode2023/SolutionsByDay/Day12/Day12.cs
@@ -0,0 +1,82 @@
+﻿using AdventOfCode2023.SolutionsByDay.Day11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.SolutionsByDay.Day12
+{
+    internal class Day12 : DaySolution
+    {
+        public long GetPart1Solution()
+        {
+            // Open the file to read
+            using var fileStream = File.OpenRead("SourceInputs/Day12.txt");
+
+            // Stream the contents
+            using var streamReader = new StreamReader(fileStream);
+
+            // Store the current line
+            string? line = null;
+
+            // Iterate over each map line until the line is null
+            long totalArrangements = 0;
+            SpringArrangementsSolver solver = new();
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                string[] lineSplit = line.Split(' ');
+                string springs = lineSplit[0];
+                string blockCountsString = lineSplit[1];
+                int[] blockCounts = Array.ConvertAll(blockCountsString.Split(','), Int32.Parse);
+                SpringArrangementsSolver.ArrangementCache.Clear();
+                totalArrangements += solver.GetNumArrangements(
+                    springs, blockCounts, 0, 0, 0
+                    );
+            }
+            return totalArrangements;
+        }
+
+        public long GetPart2Solution()
+        {
+            // Open the file to read
+            using var fileStream = File.OpenRead("SourceInputs/Day12.txt");
+
+            // Stream the contents
+            using var streamReader = new StreamReader(fileStream);
+
+            // Store the current line
+            string? line = null;
+
+            // Iterate over each map line until the line is null
+            long totalArrangements = 0;
+            SpringArrangementsSolver solver = new();
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                string[] lineSplit = line.Split(' ');
+                string springs = lineSplit[0];
+                string blockCountsString = lineSplit[1];
+
+                // In part 2, we only need copy the records five times
+                // It also states to join the copies by a '?' on the springs
+                // and join the block counts by a ','
+                springs = String.Join('?', [
+                    springs, springs, springs, springs, springs
+                    ]);
+                blockCountsString = String.Join(',', [
+                    blockCountsString, blockCountsString, blockCountsString, blockCountsString, blockCountsString
+                    ]);
+
+                int[] blockCounts = Array.ConvertAll(blockCountsString.Split(','), Int32.Parse);
+
+                // Clear the solved group arrangement cache before beginning each line's arrangement solve
+                SpringArrangementsSolver.ArrangementCache.Clear();
+
+                totalArrangements += solver.GetNumArrangements(
+                    springs, blockCounts, 0, 0, 0
+                    );
+            }
+            return totalArrangements;
+        }
+    }
+}
---
+++ src/AdventOfCode2023/SolutionsByDay/Day12/SpringArrangementsSolver.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.SolutionsByDay.Day12
+{
+    internal class SpringArrangementsSolver
+    {
+
+        public static Dictionary<string, long> ArrangementCache = [];
+
+        /// <summary>
+        /// Recursively solves a line's possible spring combinations while also utilizing a cache
+        /// to avoid re-solving a similar outcome (makes Part 2 actually solveable in under 300ms as opposed to 400 years)
+        /// </summary>
+        /// <param name="springsLine"></param>
+        /// <param name="springGroupCounts"></param>
+        /// <param name="currentPosition"></param>
+        /// <param name="currentSpringGroupCountIndex"></param>
+        /// <param name="currentLengthOfHashtagGroup"></param>
+        /// <returns></returns>
+        public long GetNumArrangements(
+            string springsLine, 
+            int[] springGroupCounts, 
+            int currentPosition, 
+            int currentSpringGroupCountIndex,
+            int currentLengthOfHashtagGroup
+            )
+        {
+            string cacheKey = String.Format("{0}_{1}_{2}", currentPosition, currentSpringGroupCountIndex, currentLengthOfHashtagGroup);
+            if (ArrangementCache.TryGetValue(cacheKey, out long numArrangements))
+            {
+                return numArrangements;
+            }
+            else
+            {
+                // Are we at the end of the springs entire line?
+                if (currentPosition == springsLine.Length)
+                {
+                    if (currentSpringGroupCountIndex == springGroupCounts.Length && currentLengthOfHashtagGroup == 0)
+                    {
+                        return 1;
+                    }
+                    else if (currentSpringGroupCountIndex == springGroupCounts.Length - 1 && springGroupCounts[currentSpringGroupCountIndex] == currentLengthOfHashtagGroup)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    long answer = 0;
+                    char[] validCharacters = ['.', '#'];
+                    foreach (char c in validCharacters)
+                    {
+                        if (springsLine[currentPosition] == c || springsLine[currentPosition] == '?')
+                        {
+                            if (c == '.' && currentLengthOfHashtagGroup == 0)
+                            {
+                                answer += GetNumArrangements(
+                                    springsLine, springGroupCounts, currentPosition + 1, currentSpringGroupCountIndex, 0
+                                    );
+                            }
+                            else if (c == '.' && currentLengthOfHashtagGroup > 0 && currentSpringGroupCountIndex < springGroupCounts.Length && springGroupCounts[currentSpringGroupCountIndex] == currentLengthOfHashtagGroup)
+                            {
+                                answer += GetNumArrangements(
+                                    springsLine, springGroupCounts, currentPosition + 1, currentSpringGroupCountIndex + 1, 0
+                                    );
+                            }
+                            else if (c == '#')
+                            {
+                                answer += GetNumArrangements(
+                                    springsLine, springGroupCounts, currentPosition + 1, 
+                                    currentSpringGroupCountIndex, currentLengthOfHashtagGroup + 1
+                                    );
+                            }
+                        }
+                    }
+                    ArrangementCache[cacheKey] = answer;
+                    return answer;
+                }
+            }
+        }
+    }
+}
