9e00a1ae6f48daae680dc7b28c2655f6d8834f63
Cole Green <17110935+nox7@users.noreply.github.com>
2023-12-14 20:04:15 -0600

    Day 14 solutions

---
+++ src/AdventOfCode2023/SolutionsByDay/Day14/Day14.cs
@@ -0,0 +1,90 @@
+﻿using AdventOfCode2023.SolutionsByDay.Day11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.SolutionsByDay.Day14
+{
+	internal class Day14 : DaySolution
+	{
+		public long GetPart1Solution()
+		{
+			// Open the file to read
+			using var fileStream = File.OpenRead("SourceInputs/Day14.txt");
+
+			// Stream the contents
+			using var streamReader = new StreamReader(fileStream);
+			string fileContents = streamReader.ReadToEnd();
+
+			string[] rockGridLines = fileContents.Split("\r\n");
+			var rockGrid = new RockGrid()
+			{
+				Lines = rockGridLines
+			};
+
+			var rocksAsDictionary = rockGrid.GetLinesAsDictionaries();
+			rockGrid.RollAllRocksNorth(ref rocksAsDictionary);
+
+			foreach (var dictionary in rocksAsDictionary.Values)
+			{
+				foreach(var symbol in dictionary.Values)
+				{
+					Console.Write(symbol);
+				}
+				Console.WriteLine();
+			}
+
+			long load = rockGrid.GetLoadFromRocks(rocksAsDictionary);
+
+			return load;
+		}
+
+		public long GetPart2Solution()
+		{
+			// Open the file to read
+			using var fileStream = File.OpenRead("SourceInputs/Day14.txt");
+
+			// Stream the contents
+			using var streamReader = new StreamReader(fileStream);
+			string fileContents = streamReader.ReadToEnd();
+
+			string[] rockGridLines = fileContents.Split("\r\n");
+			var rockGrid = new RockGrid()
+			{
+				Lines = rockGridLines
+			};
+
+			var rocksAsDictionary = rockGrid.GetLinesAsDictionaries();
+
+			long maxIterations = 1000000000;
+			List<long> fourthCycleMemory = [];
+			for (long cycleIndex = 1; cycleIndex <= maxIterations; cycleIndex++)
+			{
+				// Roll, then rotate. A cycle is 4 rolls and 4 rotations
+				for (long rotationIndex = 1; rotationIndex <= 4; rotationIndex++)
+				{
+					rockGrid.RollAllRocksNorth(ref rocksAsDictionary);
+					rockGrid.RotateRockGrid90Degrees(ref rocksAsDictionary);
+					long loadAfterRolling = rockGrid.GetLoadFromRocks(rocksAsDictionary);
+					Console.WriteLine($"Cycle {cycleIndex}. Rotation {rotationIndex}. Load: {loadAfterRolling}");
+
+					if (rotationIndex == 4 && cycleIndex < 950000000)
+					{
+						if (fourthCycleMemory.Contains(loadAfterRolling))
+						{
+							cycleIndex = maxIterations - cycleIndex;
+						}
+						else
+						{
+							fourthCycleMemory.Add(loadAfterRolling);
+						}
+					}
+				}
+			}
+
+			return 0;
+		}
+	}
+}
---
+++ src/AdventOfCode2023/SolutionsByDay/Day14/RockGrid.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023.SolutionsByDay.Day14
+{
+	internal class RockGrid
+	{
+		public required string[] Lines { get; set; }
+
+		public Dictionary<int, Dictionary<int, char>> GetLinesAsDictionaries()
+		{
+			Dictionary<int, Dictionary<int, char>> rocksGridAsDictionary = [];
+			for (int row = 0; row < Lines.Length; row++)
+			{
+				if (!rocksGridAsDictionary.TryGetValue(row, out Dictionary<int, char>? line))
+				{
+					// If there is no Dictionary for this row, create one
+					rocksGridAsDictionary[row] = [];
+				}
+
+				for (int column = 0; column < Lines[0].Length; column++)
+				{
+					rocksGridAsDictionary[row].Add(column, Lines[row][column]);
+				}
+			}
+
+			return rocksGridAsDictionary;
+		}
+
+		public void RollAllRocksNorth(ref Dictionary<int, Dictionary<int, char>> rocksGridAsDictionary)
+		{
+			/*Dictionary<int, Dictionary<int, char>> rolledDictionary = [];
+			for (int row = 0; row < Lines.Length; row++)
+			{
+				rolledDictionary[row] = 
+			}*/
+
+			for (int column = 0; column < Lines[0].Length; column++)
+			{
+				for (int _ = 0; _ < Lines.Length; _++){
+					for (int row = 0; row < Lines.Length; row++)
+					{
+						// Is this row below the top row and currently a rock?
+						// Then, is the row above row and column a free space?
+						if (rocksGridAsDictionary[row][column] == 'O' && row > 0 && rocksGridAsDictionary[row - 1][column] == '.')
+						{
+							// Modify the current row column to be an empty space now
+							rocksGridAsDictionary[row][column] = '.';
+
+							// Modify the above row to be the moved rock
+							rocksGridAsDictionary[row - 1][column] = 'O';
+						}
+					}
+				}
+			}
+		}
+
+		public long GetLoadFromRocks(Dictionary<int, Dictionary<int, char>> rockGridAsDictionary)
+		{
+			long loadFromRocks = 0;
+			for (int row = 0; row < Lines.Length; row++)
+			{
+				for (int column = 0; column < Lines[0].Length; column++)
+				{
+					if (rockGridAsDictionary[row][column] == 'O')
+					{
+						// Calculate load by getting the current 1-based index of the current row
+						loadFromRocks += Lines.Length - row;
+					}
+				}
+			}
+
+			return loadFromRocks;
+		}
+
+		public void RotateRockGrid90Degrees(ref Dictionary<int, Dictionary<int, char>> rockGridAsDictionary)
+		{
+			Dictionary<int, Dictionary<int, char>> rotatedGrid = [];
+
+			// Fill in blank rows
+			for (int row = 0; row < Lines.Length; row++)
+			{
+				rotatedGrid[row] = [];
+			}
+
+				
+			for(int row = 0; row < Lines.Length; row++)
+			{
+				for (int column = 0; column < Lines[0].Length; column++)
+				{
+					rotatedGrid[column][Lines.Length - 1 - row] = rockGridAsDictionary[row][column];
+				}
+			}
+
+			rockGridAsDictionary = rotatedGrid;
+		}
+	}
+}
